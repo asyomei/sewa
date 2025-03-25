3894e1ccd5f17b686ead60d0f13f6dd33cd3e88a
asyomei <vadim@asyomei.org>
2025-03-25 13:57:32 +0500

    fix: use https

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo '===> Create tar.gz'
 tar -czf dist.tar.gz -C dist .
 
 echo '===> Deploy'
-curl sewa.asyomei.org/deploy/sewa -F password="$DEPLOY_PASSWORD" \
+curl https://sewa.asyomei.org/deploy/sewa -F password="$DEPLOY_PASSWORD" \
   -F name="$NAME" -F dist=@dist.tar.gz -F outpath="$OUTPATH"
